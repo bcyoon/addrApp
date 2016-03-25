abf9f35fe8d71b4018ded3123c1c1e4696e2ac0f
bcyoon <near77@gmail.com>
2016-03-25 18:28:57 +0900

    개발자 액티비티 생성

--- app/src/main/java/com/study/itwise/addrapp/MainActivity.java
+++ app/src/main/java/com/study/itwise/addrapp/MainActivity.java
@@ -1,7 +1,14 @@
 package com.study.itwise.addrapp;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.study.itwise.addrapp.com.study.itwise.addrapp.yoon.YoonMainActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //윤병찬 화면 전환 버튼
+        Button goingYoonBtn = (Button) this.findViewById(R.id.goingYoonBtn);
+        goingYoonBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentYoon = new Intent(getApplicationContext(), YoonMainActivity.class);
+                startActivity(intentYoon);
+                //setContentView(R.layout.activity_yoon_main);
+            }
+        });
+
     }
+
+
+
+
 }
